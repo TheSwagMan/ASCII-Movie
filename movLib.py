4717e5db96e681e0283ab0a64817dd9097ed1bda
Thomas POTIER <theswagman@gmx.fr>
2016-11-24 13:21:39 +0100

    Update

--- movLib.py
+++ movLib.py
@@ -1,1 +1,21 @@
-import moviepy
+from moviepy.editor import *
+import os, imgLib
+from PIL import Image
+
+mym = VideoFileClip("sample.mp4", verbose=False)
+frames = list(mym.iter_frames())
+for i in range(len(frames)):
+    if i > 50:
+        break
+    tempimg = imgLib.SimplePicture(list(frames[i]), dtype="A")
+    print(tempimg)
+    tempimg.resize_width(70)
+    print(tempimg)
+    tempimg.to_ascii().save("temp_frames/ascii" + str(i) + ".txt")
+
+exit()
+mym.write_images_sequence("temp_frames/frame%05d.png", verbose=False)
+for imname in os.listdir("temp_frames"):
+    tempimg = imgLib.SimplePicture(Image.open("temp_frames/" + imname).getdata(), dtype="A")
+    os.remove("temp_frames/" + imname)
+    tempimg.to_ascii().save("temp_frames/ascii" + imname + ".txt")
